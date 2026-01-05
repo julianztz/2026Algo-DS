a1e34ebd8ca71be61625a0bac8be46adee05e739
Your Name <your.email@example.com>
2026-01-04 21:04:58 -0500

    leetcode 75 update

--- 2025_need_review.py
+++ 2025_need_review.py
@@ -50,7 +50,7 @@ def smallerNumbersThanCurrent(nums: List[int]) -> List[int]:
     return res
 
 
-smallerNumbersThanCurrent([5,0,10,0,10,6])
+# smallerNumbersThanCurrent([5,0,10,0,10,6])
 
 
 # lc 41 First Missing Positive
@@ -164,3 +164,157 @@ def findMaxAverage(nums: List[int], k: int) -> float:
     
 # findMaxAverage([3,3,4,3,0],3)
 findMaxAverage([9,7,3,5,6,2,0,8,1,9],6)
+
+
+# 1071 Greatest Common Divisor of Strings
+'''
+正确思路：
+1. 如果 str1 + str2 != str2 + str1，说明没有公共因子，返回 ""
+2. 找到两个字符串长度的最大公约数（GCD）
+3. 返回 str1[:gcd_length]（或 str2[:gcd_length]）
+
+关键观察：
+- 如果两个字符串有公共因子，那么 str1 + str2 == str2 + str1
+- GCD 的长度就是两个字符串长度的最大公约数
+'''
+def gcdOfStrings(str1: str, str2: str) -> str:
+    # 关键检查：如果 str1 + str2 != str2 + str1，说明没有公共因子
+    if str1 + str2 != str2 + str1:
+        return ''
+    
+    # 计算两个字符串长度的最大公约数
+    def gcd(a: int, b: int) -> int:
+        while b:
+            a, b = b, a % b
+        return a
+    
+    gcd_length = gcd(len(str1), len(str2))
+    return str1[:gcd_length]
+
+
+# 345. Reverse Vowels of a String
+def reverseVowels(s: str) -> str:
+    res = ""
+
+    vowels = ['a','e','i','o','u']
+
+    p = len(s) - 1 
+    for c in s:
+        if c.lower() not in vowels:
+            res += c
+        else:
+            while s[p].lower() not in vowels:
+                p -= 1
+            res += s[p]
+            p -= 1
+
+    print(res)
+    return res   
+     
+# reverseVowels("IceCream")
+
+
+# 238 product of array except for self
+'''
+suffix * prefix
+'''
+def productExceptSelf(nums: List[int]) -> List[int]:
+    res = [1] * (len(nums))
+    for i in range(1,len(nums)):
+        res[i] = nums[i-1] * res[i-1]
+
+    suffix = 1
+    for i in range(len(nums)-2,-1,-1):
+        suffix = nums[i+1] * suffix
+        res[i] *= suffix
+
+    return res
+
+productExceptSelf([1,2,3,4])
+
+
+# 334. Increasing Triplet Subsequence
+'''
+找三个递增元素
+'''
+def increasingTriplet(nums: List[int]) -> bool:
+    res = []
+    first = float("inf")
+    second = float("inf")
+    for n in nums:
+        if n <= first:
+            first = n
+        elif n <= second:
+            second = n
+            mark_first = first
+            
+        else:
+            res = [mark_first, second, n]
+            print(res)
+            return True        # 存在一个比前两个数字大
+
+    return False
+
+# increasingTriplet([4,5,0,6])
+# increasingTriplet([40,50,3,2,3,60])
+
+
+# 443. string compression
+'''
+双(三)指针 -- 快慢指针
+
+slow/start -- group起点的位置
+write - 写入相同char长度位置
+fast -- 新char/group终点+1的位置
+'''
+def compress(chars: List[str]) -> int:
+    fast = 0
+    slow = 0  # group 起点
+    write = 0  # 写入位置
+
+    n = len(chars)
+    while fast <= n:
+        if fast==n or chars[fast] != chars[slow]:
+            
+            chars[write] = chars[slow]
+            write += 1
+
+            group_len = fast - slow
+            if group_len > 1:
+                for c in str(group_len):
+                    chars[write] = c
+                    write += 1
+            slow = fast
+        
+        fast += 1
+    return write
+
+c = ["a","a","b","b","c","c","c"]
+c2 = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+c3 = ["a","b","b","b","b","b","b","b","b","b","b","b","b","a","a","a","a"]
+c4 = ['a','a']
+c5 = ['a','b','c']
+c6 = ['a','a','a','b']
+# compress(c)
+
+
+# 283. Move Zeroes 
+'''
+指针职责要单一
+所有non-zero前移，最后剩下的都是0
+
+'''
+def moveZeroes(nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    zero_seperator = 0           # 分界线 123 | 000  
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[i], nums[zero_seperator] = nums[zero_seperator], nums[i]
+            zero_seperator += 1
+
+nums = [0,1,0,3,12]          
+nums2 = [1,2]
+nums3 = [1,0,1]
+moveZeroes(nums3)
